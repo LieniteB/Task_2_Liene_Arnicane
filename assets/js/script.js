a98377bbdaa37ae795d006741e4ec769a2c1740b
LieniteB <liene.arnicane@gmail.com>
2022-05-29 19:23:07 +0300

    3.uzdevums

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//navigation - mobile menu
 const mobileBtn = document.getElementById("mobile-cta")
         nav = document.querySelector("nav")
         mobileBtnExit = document.getElementById("mobile-exit");
@@ -7,6 +8,37 @@ const mobileBtn = document.getElementById("mobile-cta")
         })
         mobileBtnExit.addEventListener("click", () => {
             nav.classList.remove("menu-btn");
-        })
+        });
+
+//99 iedvesmas stāsti
+window.addEventListener("load", () => {
+    const start = 10;
+    const end = 99;
+    var data = [...Array(end - start + 1).keys()].map(x => x+ start);
+    var container = document.getElementById("grid");
+    var more = document.getElementById("more");
+    for (var i of data) {
+        var cell = document.createElement("div");
+        var p = document.createElement("p");
+        p.innerHTML = i;
+        cell.className = "cell";
+        if (i > 18) {
+            cell.appendChild(p);
+            more.appendChild(cell);
+        }else{
+        cell.appendChild(p);
+        container.appendChild(cell);
+    }
+    }
+});
+
+function moreStories() {
+    var x = document.getElementById("more");
+    if (x.style.display === "none") {
+        x.style.display = "grid";
+    } else {
+        x.style.display = "none";
+    }
+}
 
 
